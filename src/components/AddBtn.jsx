3724ce0c265a2fb9fae71ed5f7adac020520b3c0
Amangoua Kacou Ange-Thierry <104840429+amangouste225@users.noreply.github.com>
2024-08-14 14:35:12 +0000

    Better Skeleton Display

--- src/components/AddBtn.jsx
+++ src/components/AddBtn.jsx
@@ -1,7 +1,13 @@
-export default function AddBtn({ children, onclick }) {
+import { GoSync } from "react-icons/go";
+
+export default function AddBtn({ children, onclick, loading }) {
   return (
-    <button onClick={onclick} className="border-2 px-5 py-1 border-black">
-      {children}
+    <button
+      disabled={loading}
+      onClick={onclick}
+      className="border-2 px-5 py-1 border-black"
+    >
+      {loading ? <GoSync className="animate-spin" /> : children}
     </button>
   );
 }
--- src/components/ListOfUsers.jsx
+++ src/components/ListOfUsers.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { fetchUsers } from "../store/store";
 import { RiDeleteBin6Line } from "react-icons/ri";
 import { IoIosArrowDown } from "react-icons/io";
@@ -7,65 +7,59 @@ import Skeleton from "react-loading-skeleton";
 import { removeUser } from "../store/thunks/removeUser";
 import AddBtn from "./AddBtn";
 import { addUser } from "../store/thunks/addUser";
+import { useThunk } from "../hooks/use-thunks";
 
 export default function ListOfUsers() {
-  const [isLoadingUsers, setLoadingUsers] = useState(false);
-  const [loadingUsersError, setLoadingUsersError] = useState(null);
-  const [isCreatingUser, setIsCreatingUser] = useState(false);
-  const [creatingUserError, setCreatingUserError] = useState(null);
+  const [doFetchUsers, isLoadingUsers, loadingUsersError] =
+    useThunk(fetchUsers);
 
-  const dispatch = useDispatch();
+  const [doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser);
+
   const { data } = useSelector((state) => state.users);
 
   const handleAddUser = () => {
-    setIsCreatingUser(true);
-    dispatch(addUser())
-      .unwrap()
-      .catch((err) => setCreatingUserError(err))
-      .finally(() => setIsCreatingUser(false));
+    doCreateUser();
   };
 
   useEffect(() => {
-    setLoadingUsers(true);
-    dispatch(fetchUsers())
-      .unwrap()
-      .catch((err) => setLoadingUsersError(err))
-      .finally(() => setLoadingUsers(false));
-  }, [dispatch]);
+    doFetchUsers();
+  }, [doFetchUsers]);
 
-  if (isLoadingUsers)
-    return <Skeleton className="h-10 my-2" count={data.length} />;
+  let content;
 
-  if (loadingUsersError) return <div> Error fetching data...</div>;
-
-  const renderedList = data.map((user) => {
-    return (
-      <li key={user.id} className="mb-2 border rounded">
-        <div className="flex p-2 justify-between items-center cursor-pointer">
-          <div className="flex items-center gap-2">
-            <RiDeleteBin6Line onClick={() => removeUser(user.id)} />
-            {user.name}
+  if (isLoadingUsers) {
+    content = <Skeleton className="h-10 my-2" count={data.length} />;
+  } else if (loadingUsersError) {
+    content = <div> Error fetching data...</div>;
+  } else {
+    content = data.map((user) => {
+      return (
+        <li key={user.id} className="mb-2 border rounded">
+          <div className="flex p-2 justify-between items-center cursor-pointer">
+            <div className="flex items-center gap-2">
+              <RiDeleteBin6Line onClick={() => removeUser(user.id)} />
+              {user.name}
+            </div>
+            <IoIosArrowDown />
           </div>
-          <IoIosArrowDown />
-        </div>
-      </li>
-    );
-  });
+        </li>
+      );
+    });
+  }
 
   return (
     <div className="flex-col flex gap-5">
       <div className="flex items-center justify-between">
         <h2 className="font-bold">List of Users</h2>
-        {isCreatingUser ? (
-          "Creating User..."
-        ) : (
-          <AddBtn onclick={handleAddUser}>+ Add User</AddBtn>
-        )}
+
+        <AddBtn loading={isCreatingUser} onclick={handleAddUser}>
+          + Add User
+        </AddBtn>
 
         {creatingUserError && "Error creating User..."}
       </div>
 
-      <ul className="space-y-3">{renderedList}</ul>
+      <ul className="space-y-3">{content}</ul>
     </div>
   );
 }
